84ee587b4e0eddf3a44ac3e531a565597e94d725
UsualSpec <98665326+UsualSpec@users.noreply.github.com>
2024-10-26 10:14:12 +0200

    Improve Zabbix Setup * Allow separate Zabbix server * Enable active and passive monitoring

--- client/serverIpConfig.sh
+++ client/serverIpConfig.sh
@@ -1,5 +1,6 @@
 # Edit this file to use a different IP and host 
 REMOTEHOST="ee-tik-nsgvm057.ethz.ch"
+REMOTEZABBIXHOST="ee-tik-nsgvm057.ethz.ch"
 REMOTEIP="129.132.31.132"
 REMOTEIP6="2001:67c:10ec:2a40::31"
 JUMPHOST="-J jacobr@pc-10587.ethz.ch:56789"
