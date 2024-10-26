a76b630864e2b3f3e5d5bb9e97a4206f86003f09
UsualSpec <98665326+UsualSpec@users.noreply.github.com>
2024-10-26 10:27:55 +0200

    Make zabbix ip less dependent

--- client/deploy.sh
+++ client/deploy.sh
@@ -15,6 +15,9 @@ if ! [[ "${AUTOPOWERNUMBER}" =~ ^[0-9]+$ ]]; then
   exit 1
 fi
 
+# set timezone
+timedatectl set-timezone "Europe/Zurich"
+
 # install needed services (including adding zabbix)
 pushd /tmp
 wget https://repo.zabbix.com/zabbix/6.0/raspbian/pool/main/z/zabbix-release/zabbix-release_6.0-5+debian12_all.deb
@@ -48,8 +51,10 @@ echo "${REMOTEIP}  ${REMOTEHOST}" >> /etc/hosts
 # password as per https://stackoverflow.com/questions/44376846/creating-a-password-in-bash
 # This will not give special characters. May still be worth escaping in future
 PGPASSWORD=$(cat /dev/urandom | tr -dc A-Za-z0-9~_- | head -c 60 && echo)
+# Delete autopower_client user if already exists and create database + schema
+sudo -u postgres psql -d postgres -c "DROP ROLE IF EXISTS autopower_client;"
 sudo -u postgres psql -d postgres -c "CREATE USER autopower_client WITH PASSWORD '${PGPASSWORD}';"
-sudo -u postgres psql -d postgres -c "CREATE DATABASE autopower_client;"
+sudo -u postgres psql -d postgres -c "CREATE DATABASE IF NOT EXISTS autopower_client;"
 sudo -u postgres psql -d autopower_client -a -f client_db_schema.sql
 
 # Create group to allow access to on board leds
@@ -98,13 +103,13 @@ systemctl enable reversessh
 # copy udev rule to allow leds group to access leds
 cp deploy/99-leds.rules /etc/udev/rules.d/99-leds.rules
 
-# set timezone
-timedatectl set-timezone "Europe/Zurich"
-# copy ssh key
-echo "Copying ssh key"
-mkdir /home/ethditet/.ssh
-cat deploy/ssh_key.pub >> /home/ethditet/.ssh/authorized_keys
-chown ethditet:ethditet /home/ethditet/.ssh/authorized_keys
+# copy ssh key if set
+if [[ -f "deploy/ssh_key.pub" ]]; then
+  echo "Copying ssh key"
+  mkdir /home/ethditet/.ssh
+  cat deploy/ssh_key.pub >> /home/ethditet/.ssh/authorized_keys
+  chown ethditet:ethditet /home/ethditet/.ssh/authorized_keys
+fi
 
 cp deploy/sshd_autopower.conf /etc/ssh/sshd_config.d/sshd_autopower.conf
 
@@ -122,14 +127,16 @@ chown root:zabbix /etc/zabbix/psk.psk
 systemctl restart zabbix-agent2
 systemctl enable zabbix-agent2
 
-# enable firewall and only allow ssh on port 21092 and zabbix agent to ${REMOTEIP}/${REMOTEIP6} - this assumes that zabbix is installed on ${REMOTEIP}/${REMOTEIP6}
+# enable firewall and only allow ssh on port 21092 and zabbix agent to ${REMOTEZABBIXIP}/${REMOTEZABBIXIP6} - this assumes that zabbix is installed on ${REMOTEZABBIXIP}/${REMOTEZABBIXIP6}
 ufw default deny incoming
 ufw default allow outgoing
 ufw allow 21092/tcp
-ufw allow from "${REMOTEIP}" to any port 10050 proto tcp
-ufw allow from "${REMOTEIP6}" to any port 10050 proto tcp
+ufw allow from "${REMOTEZABBIXIP}" to any port 10050 proto tcp
+if [ -n "${REMOTEZABBIXIP6}" ]; then
+  ufw allow from "${REMOTEZABBIXIP6}" to any port 10050 proto tcp
+fi
 ufw logging off
 ufw --force enable
 
-echo "Please copy /etc/mmclient/client_${DEVICENAME}.csr to the server and sign the certificate request. Afterwards setup zabbix monitoring with the psk in ./zabbix_psk.psk"
+echo "Please copy /etc/mmclient/client_${DEVICENAME}.csr to the server and sign the certificate request. Afterwards check if Zabbix autoregistration succeeded"
 
--- client/serverIpConfig.sh
+++ client/serverIpConfig.sh
@@ -1,7 +1,10 @@
 # Edit this file to use a different IP and host 
 REMOTEHOST="ee-tik-nsgvm057.ethz.ch"
-REMOTEZABBIXHOST="ee-tik-nsgvm057.ethz.ch"
 REMOTEIP="129.132.31.132"
 REMOTEIP6="2001:67c:10ec:2a40::31"
+# Zabbix config
+REMOTEZABBIXHOST="ee-tik-nsgvm057.ethz.ch"
+REMOTEZABBIXIP="129.132.31.132"
+REMOTEZABBIXIP6="2001:67c:10ec:2a40::31"
 JUMPHOST="-J jacobr@pc-10587.ethz.ch:56789"
 NOKEYCHECK="-o 'StrictHostKeyChecking no'"
