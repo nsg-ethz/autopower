521305f4e23e58aee1e858718c15efd199830f37
UsualSpec <98665326+UsualSpec@users.noreply.github.com>
2025-02-17 11:21:51 +0100

    Switch over to always use the external jumphost

--- client/serverIpConfig.sh
+++ client/serverIpConfig.sh
@@ -8,7 +8,6 @@ REMOTEIP6="2001:67c:10ec:2a40::31"
 REMOTEZABBIXHOST="ee-tik-nsgvm057.ethz.ch"
 REMOTEZABBIXIP="129.132.31.132"
 REMOTEZABBIXIP6="2001:67c:10ec:2a40::31"
-JUMPHOST="" #-J jacobr@pc-10587.ethz.ch:56789"
 # ^ if I jumphost, then the no key checking does not get applied to the correct SSH session. 
 # Removing the jumphost is the simplest, given that I most often flash devices within the ETH network.
 NOKEYCHECK="-o StrictHostKeyChecking=no"
